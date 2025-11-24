0f4dac960008da5be46d9662fbd9f2d0219c2a64
Taha Tariq <tahatariqf@gmail.com>
2025-11-23 16:32:49 -0800

    Task 01 done

---
+++ CodeAlpha intership challenges/Hangman Game.py
@@ -0,0 +1,31 @@
+word = list("tahat".lower())
+flag = True
+while flag: 
+    lives = 5
+    answer = ["_" for i in range(len(word))]
+    print("Guess the following word by substituting letters: ")
+    for i in answer:
+        print(i, end = " ")
+    print()
+    for i in range(lives):
+        if "".join(answer) != "".join(word):
+            guess_letter = input("Enter a letter: ").lower()
+            for index, letter in enumerate(word):
+                if guess_letter == letter:
+                    answer[index] = guess_letter
+            for i in answer:
+                print(i, end = " ")
+            lives = lives-1
+            print(f"You have {lives} left")
+        else:
+            break
+    if "".join(answer) == "".join(word):
+        print("You won!")
+    else:
+        print("You lost")
+    again = input("Do you want to  play again? (y/n): ").lower()
+    if again == "y":
+        flag = True
+    else:
+        print("Thankyou for playing with us!")
+        flag = False
